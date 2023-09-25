ede4cd7a466727aa08e00c49783bd0d20d9c5d18
Juan408AR <aguirre.juanmp15@gmail.com>
2023-09-24 22:40:49 -0700

    Added some CRUD functionality to the goalController

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController');
 
+const {protect} = require('../middleware/authMiddleware')
+
 // the rest of the path was specified in server.js
-router.route('/').get(getGoals).post(setGoals);
-router.route('/:id').put(updateGoals).delete(deleteGoals);
+router.route('/').get(protect, getGoals).post(protect, setGoals);
+router.route('/:id').put(protect, updateGoals).delete(protect, deleteGoals);
 
 module.exports = router;
