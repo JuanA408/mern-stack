d05e86d82951e7b33cea97d28a7c1886ab9b459d
Juan408AR <aguirre.juanmp15@gmail.com>
2023-10-22 10:51:21 -0700

    Preparing for netlify

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 3000;
 
 connectDB();
@@ -10,15 +11,25 @@ connectDB();
 const app = express();
 
 // Body-parser middleware
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes.js"));
 
-app.use('/api/goals', require('./routes/goalRoutes'));
-app.use('/api/users', require('./routes/userRoutes.js'));
+// Frontend serving
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+    app.get('/', (req, res) => res.send('Set to production'))
+}
 
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
